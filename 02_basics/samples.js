069d7795c2438bb2256c91c4aff889d95b923c8c
Felipe Gaete <felipegaete@localhost.localdomain>
2020-04-28 21:02:23 -0400

    variables 01

---
+++ 02_basics/samples.js
@@ -0,0 +1,7 @@
+var decision = true;
+var numero = 14;
+var nombre = "Felipe";
+var apellido = "Gaete";
+console.log(decision);
+console.log("Número es: " + numero);
+console.log("Hola " + nombre + " " + apellido);
---
+++ 02_basics/samples.ts
@@ -0,0 +1,8 @@
+var decision: boolean = true;
+let numero: number = 14;
+let nombre: string = "Felipe";
+let apellido: string = "Gaete";
+
+console.log(decision);
+console.log("Número es: " + numero);
+console.log("Hola " + nombre + " " + apellido);
